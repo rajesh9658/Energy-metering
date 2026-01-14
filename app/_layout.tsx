68b3c630ace86abbb64cfd750d5cd57208f0cf3c
snajay <sanjay9664>
2026-01-14 15:05:49 +0530

    energy-metering

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
-// app/_layout.tsx
 
 import { Stack, Redirect } from "expo-router";
-// AuthProvider को context फोल्डर से इम्पोर्ट करें
+
 import { AuthProvider, useAuth } from "./context/AuthContext"; 
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { useEffect } from "react";
@@ -28,13 +27,13 @@ function LayoutContent() {
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* अगर user लॉग इन नहीं है, तो उसे (auth) पर रीडायरेक्ट करें */}
+      
       <Stack.Screen name="(tabs)" redirect={!user} />
       
-      {/* अगर user लॉग इन है, तो उसे (tabs) पर रीडायरेक्ट करें */}
+      
       <Stack.Screen name="(auth)" redirect={!!user} />
 
-      {/* रूट में मौजूद फ़ाइलों को रीडायरेक्ट करें */}
+  
       <Stack.Screen name="index" redirect />
       <Stack.Screen name="login" redirect />
       <Stack.Screen name="signup" redirect />
